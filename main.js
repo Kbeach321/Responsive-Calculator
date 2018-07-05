8832a83d35cb41c7d2c8166678d813f43bc64e52
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-05 13:59:21 -0400

    cleanup

---
+++ main.js
@@ -0,0 +1,21 @@
+// variables //
+var calculation = []
+
+// Number Functions //
+function pushNumber {
+  alert();
+}
+
+// Operator Functions //
+function pushOperator {
+  alert();
+}
+
+// Calculate Functions //
+function calculate {
+  alert()
+}
+
+// Numbers pushed to Clalculation
+
+// Using for loop alert() the Calcultation when = (calculate) is pressed
