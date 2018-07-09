f70a412feb08672b7aa2ed8637a74fc46b7822be
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-09 17:06:07 -0400

    finalized

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
+
 // variables //
-var calculation = []
+var calculationArray = [];
+var result = 0;
+var operators = ['+', '*', '/', '-'];
+var operator='', num1='', num2='';
 
 // Number Functions //
-function pushNumber {
-  alert();
+var $numbers = document.querySelectorAll(".num");
+for (i=0; i < $numbers.length; i++) {
+  $numbers[i].addEventListener("click",pushNumber);
+};
+function pushNumber() {
+  calculationArray.push(event.target.textContent);
+  console.log(calculationArray);
 }
 
 // Operator Functions //
-function pushOperator {
-  alert();
+var $operator = document.querySelectorAll(".operator");
+for (i=0; i < $operator.length; i++) {
+  $operator[i].addEventListener("click",pushOperator);
+};
+function pushOperator() {
+  calculationArray.push(event.target.textContent);
+  console.log(calculationArray);
 }
 
-// Calculate Functions //
-function calculate {
-  alert()
+// Calculate Function -- combines the numbers //
+document.querySelector(".calculate").addEventListener("click", calculate)
+function calculation(item) {
+
+  if(operators.indexOf(item) !== -1) {
+    operator = item;
+  } else if(operator.length === 1){
+    num2 += item;
+  } else{
+    num1 += item;
+  }
+}
+function calculate() {
+  calculationArray.forEach(calculation);
+  if(operator === '+') {
+    result = Number(num1) + Number(num2);
+  } else if (operator === '-') {
+    result = Number(num1) - Number(num2);
+  } else if (operator === '*') {
+    result = Number(num1) * Number(num2);
+  } else if (operator === '/') {
+    result = Number(num1) / Number(num2);
+  }
+document.querySelector(".result_text").textContent = result
 }
 
-// Numbers pushed to Clalculation
-
-// Using for loop alert() the Calcultation when = (calculate) is pressed
+// clearAll Function -- resets the page//
+document.querySelector("#allClear").addEventListener("click", clearAll)
+function clearAll() {
+  window.location.reload();
+}
